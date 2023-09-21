356b511c03f85107d69a3f74879768826b3706cf
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-09-22 07:58:42 +0800

    Refactor RegionRepository

--- Controller/LocationRepository.cs
+++ Controller/LocationRepository.cs
@@ -17,19 +17,20 @@ public class LocationRepository : Repository<Location>
     }
 
     public string Insert(string streetAddress, string postalCode, string city, string stateProvince, string countryID) {
-        List<SqlParameter> parameters = new List<SqlParameter>();
-
-        parameters.Add(new SqlParameter("@id", base.GetLastRow().ID + 1));
-        parameters.Add(new SqlParameter("@street_address", streetAddress));
-        parameters.Add(new SqlParameter("@postal_code", postalCode));
-        parameters.Add(new SqlParameter("@city", city));
-        parameters.Add(new SqlParameter("@state_province", stateProvince));
-        parameters.Add(new SqlParameter("@country_id", countryID));
+        int id = base.GetLastRow().ID + 1;
+        List<SqlParameter> parameters = setParameters(id, streetAddress, postalCode, city, stateProvince, countryID);
 
         return base.Insert(parameters);
     }
 
     public string Update(int id, string streetAddress, string postalCode, string city, string stateProvince, string countryID)
+    {
+        List<SqlParameter> parameters = setParameters(id, streetAddress, postalCode, city, stateProvince, countryID);
+              
+        return base.Update(parameters);
+    }
+
+    public List<SqlParameter> setParameters(int id, string streetAddress, string postalCode, string city, string stateProvince, string countryID)
     {
         List<SqlParameter> parameters = new List<SqlParameter>();
 
@@ -40,6 +41,6 @@ public class LocationRepository : Repository<Location>
         parameters.Add(new SqlParameter("@state_province", stateProvince));
         parameters.Add(new SqlParameter("@country_id", countryID));
 
-        return base.Update(parameters);
+        return parameters;
     }
 }
